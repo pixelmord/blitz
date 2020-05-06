e831239f8279dda0e4844584822a8487c4e2018f
merelinguist <merelinguist@users.noreply.github.com>
2020-05-06 09:41:34 +0100

    Bundle Inter

--- packages/gui/app/pages/_app.tsx
+++ packages/gui/app/pages/_app.tsx
@@ -1,13 +1,11 @@
-import '../css/tailwind.css'
+import 'styles/fonts.css'
+import 'styles/main.css'
 
 import {Head} from 'blitz'
 import {AppProps} from 'next/app'
 
 export default ({Component, pageProps}: AppProps) => (
   <>
-    <Head>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
     <div className="min-h-screen font-sans antialiased text-gray-900 bg-gray-100">
       <Component {...pageProps} />
     </div>
--- packages/gui/tailwind.config.js
+++ packages/gui/tailwind.config.js
@@ -1,11 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  purge: false,
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
